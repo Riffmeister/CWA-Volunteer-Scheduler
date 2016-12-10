f181369cb9d0e79bd76b139c6942ea8d50ed7399
Riffmeister <niri5023@colorado.edu>
2016-12-10 08:15:36 -0700

    Added less to the project

--- src/app/components/login.js
+++ src/app/components/login.js
@@ -2,50 +2,53 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './../shared/header';
 
+require('./style.less')
+
 class Login extends React.Component {
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			error: '',
-			email: '',
-			password: ''
-		}
-	}
-	//will need an action and method="post" on this form tag to direct to server
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: '',
+            email: '',
+            password: ''
+        }
+    }
+    //will need an action and method="post" on this form tag to direct to server
 
-   render() {
-    return <div>
-		<h1 class = "test">Login</h1>
-			<form ref='login'>
-		  		<label>Email:</label>
-		  			<input ref='email' type="text" autoFocus id='email'></input>
-				<br></br>
-				<label>Password:</label>
-					<input ref='password' type="text" id='password'></input>
-				<br></br>
+    render() {
+        return (
+            <section id='login'>
+                <h1>Login</h1>
+                <form ref='login'>
+                    <label>Email:</label>
+                    <input ref='email' type="text" autoFocus id='email'></input>
+                    <br></br>
+                    <label>Password:</label>
+                    <input ref='password' type="text" id='password'></input>
+                    <br></br>
+                    <button type="submit" onClick={this._handleSubmit.bind(this)}>Login</button>
+                    <button type="submit">Sign Up</button>
+                </form>
+            </section>
+        )
+    }
 
-					<button type="submit" onClick={this._handleSubmit.bind(this)}>Login</button>
-					<button type="submit">Sign Up</button>
-			</form>
-    	</div>
-  }
-
-  _handleSubmit(event) {
-	  const email = this.refs.email.value
-	  const password = this.refs.password.value
-	  if (email === "") {
-		  alert("Please Enter a Value for Email")
-		  return false
-	  } else if (password === "") {
-		  alert("Please Enter a Value for Password")
-		  return false
-	  } else {
-		  const email = this.refs.email.value
-		  const password = this.refs.password.value
-		  console.log('email', email, 'password', password)
-		  this.refs.login.reset()
-	  }
-	}
+    _handleSubmit(event) {
+        const email = this.refs.email.value
+        const password = this.refs.password.value
+        if (email === "") {
+            alert("Please Enter a Value for Email")
+            return false
+        } else if (password === "") {
+            alert("Please Enter a Value for Password")
+            return false
+        } else {
+            const email = this.refs.email.value
+            const password = this.refs.password.value
+            console.log('email', email, 'password', password)
+            this.refs.login.reset()
+        }
+    }
 }
 export default Login;
--- src/app/index.js
+++ src/app/index.js
@@ -2,9 +2,15 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import { Router, Route, hashHistory } from 'react-router'
 import Login from './components/login'
+import Header from './shared/header'
 
 ReactDOM.render((
-	<Router history={hashHistory}>
-		<Route path="/" component={Login}/>
-	</Router>
+	<section id='cwa-app'>
+		<Header/>
+		<Router history={hashHistory}>
+			<Route path="/" component={Login}/>
+				<Route path="/Header" component={Header}/>
+		</Router>
+		<Header/>
+</section>
 ), document.querySelector('section'));
