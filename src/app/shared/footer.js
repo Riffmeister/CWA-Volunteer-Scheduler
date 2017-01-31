fca286036c6223d8fce5c5e52ad2c3b74c5cd917
dylanlcole <dylan.cole5652@gmail.com>
2017-01-31 16:30:43 -0700

    added first media query, madee basic footer changes

--- src/app/shared/footer.js
+++ src/app/shared/footer.js
@@ -7,7 +7,7 @@ class Footer extends React.Component {
 		<section className='menu'>
 			<div id='footer' className='content-container'>
 				<ul>
-					<li><a href="http://www.facebook.com/cwaboulder" target="out" title="Link to Conference on World Affairs Facebook Page"><i className="fa fa-facebook-square fa-2x" aria-hidden="true"></i></a></li>
+					<li><a href="http://www.facebook.com/cwaboulder" target="out" title="Link to Conference on World Affairs Facebook Page">Facebook</a></li>
 					<li><a>Twitter</a></li>
 					<li><a>Youtube</a></li>
 					<li><a>Contact Us</a></li>
