b4b37861396aaaa1c1d62c270f81d04270d0ed5c
dylanlcole <dylan.cole5652@gmail.com>
2017-02-06 14:48:11 -0700

    fixed whitespace under the body when viewed on mobile
    fixed whitespace under the body when viewed on mobile
    
    added contact info, and modified mobile-css
    
    minor footer css tweaks and removed unneeded code
    
    start of footer hamburger nav work
    
    changed footer button size
    
    fixed id issue and added footer hamburger nav without functionacility
    
    fixed some whitespace and changed menu toggle to button
    
    setup toggleFooter function, needs implementation
    
    modified display text
    
    found solution to footer hide issue and moved footer css to footer.less
    
    cleaned up html and css
    
    initial commit and move header css to header.less
    
    some footer fix
    
    fixed some code format in mobile css classes
    
    removed other import in header
    
    did git fetch
    
    fixed whitespace under the body when viewed on mobile
    
    fixed whitespace under the body when viewed on mobile
    
    added contact info, and modified mobile-css
    
    start of footer hamburger nav work
    
    fixed id issue and added footer hamburger nav without functionacility
    
    fixed some whitespace and changed menu toggle to button
    
    modified display text
    
    found solution to footer hide issue and moved footer css to footer.less
    
    some footer fix

--- src/app/shared/footer.js
+++ src/app/shared/footer.js
@@ -1,20 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+require('./footer.less')
 class Footer extends React.Component {
+
+  constructor() {
+      super();
+      this.state = {
+          displayFooter: 'hide-mobile'
+      }
+  }
+
   render() {
     return (
 		<section className='menu'>
-			<div id='footer' className='content-container'>
-				<ul>
-					<li><a href="http://www.facebook.com/cwaboulder" target="out" title="Link to Conference on World Affairs Facebook Page">Facebook</a></li>
-					<li><a>Twitter</a></li>
-					<li><a>Youtube</a></li>
-					<li><a>Contact Us</a></li>
-				</ul>
+      <footer className='content-container clearfix'>
 
-		    </div>
+			<div id='links-footer'>
+				<ul>
+          <li id="footer-visible-menu"><button  onClick={this._toggleMenu.bind(this)}><i className="fa fa-bars"></i>Social Media Links<i className="fa fa-bars"></i></button></li>
+          <div className={this.state.displayFooter}>
+          <div id="footer-info">
+  					<li><a href="http://www.facebook.com/cwaboulder" target="out" title="Conference on World Affairs Facebook"><i className="fa fa-home"></i>Facebook</a></li>
+  					<li><a href="https://twitter.com/cwaboulder" target="out" title="Conference on World Affairs Twitter"><i className="fa fa-twitter-square"></i>Twitter</a></li>
+  					<li><a href="https://www.youtube.com/channel/UCBcqpKo9jqETiNd-FwyALoA" target="out" title="Conference on World Affairs Youtube"><i className="fa fa-youtube-play"></i>Youtube</a></li>
+            <li><a href="https://www.instagram.com/cwaboulder/" target="out" title="Conference on World Affairs Instagram"><i className="fa fa-instagram"></i>Instagram</a></li>
+            <li><a href="http://www.colorado.edu/cwa/" target="out" title="Conference on World Affairs Home"><i className="fa fa-home"></i>CWA Home</a></li>
+          </div>
+        </div>
+				</ul>
+		  </div>
+      </footer>
 		</section>)
+  }
+
+
+  _toggleMenu(){
+    if(this.state.displayFooter == 'hide-mobile'){
+      this.setState({displayFooter: 'make-visible-mobile'});
+    } else {
+      this.setState({displayFooter: 'hide-mobile'});
+    }
   }
 }
 
