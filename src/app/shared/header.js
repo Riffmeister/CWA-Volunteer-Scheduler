fa11ead6b46077df13018dc5cc0a75f82435a896
dylanlcole <dylan.cole5652@gmail.com>
2017-02-15 18:12:55 -0700

    initial commit and move header css to header.less

--- src/app/shared/header.js
+++ src/app/shared/header.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { browserHistory } from 'react-router'
-
+require('./header.less')
 class Header extends React.Component {
   render() {
     return (
