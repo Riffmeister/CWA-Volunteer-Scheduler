6f7c2d9626647eebd8ad89651306345bc8af5ffa
dylanlcole <dylan.cole5652@gmail.com>
2017-03-17 13:23:43 -0600

    added content to support page

--- src/app/components/shared/support.js
+++ src/app/components/shared/support.js
@@ -1,10 +1,21 @@
 import React from 'react'
 
+require('./support.less')
+
 class Support extends React.Component {
   render() {
     return (
-      <section>
+      <section id="support">
         <h2>Support</h2>
+        <article>
+          <h3>Conference on World Affairs Problems/Issues</h3>
+          <p>For general questions about the conference or events, please contact The Conference on World Affairs directly at <a href="mailto:cwa@colorado.edu">cwa@colorado.edy</a> or by phone at (303) 492-2525</p>
+        </article>
+        <article>
+          <h3>Website Problems/Issues</h3>
+          <p>For any issues with the Volunter Manament System Application, please contanct the VMS Team at <a href="mailto:cwajazzvms@gmail.com">cwajazzvms@gmail.com</a>.</p>
+          <p>Please include a destription of your issue and any screenshots/images that display your problem.</p>
+        </article>
       </section>
     )
   }
