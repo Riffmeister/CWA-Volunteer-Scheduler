6a002519363aaac6d2edffe7a6e7f2e4e3553173
dylanlcole <dylan.cole5652@gmail.com>
2017-03-17 13:43:54 -0600

    fixed spelling issues

--- src/app/components/shared/support.js
+++ src/app/components/shared/support.js
@@ -8,12 +8,12 @@ class Support extends React.Component {
       <section id="support">
         <h2>Support</h2>
         <article>
-          <h3>Conference on World Affairs Problems/Issues</h3>
-          <p>For general questions about the conference or events, please contact The Conference on World Affairs directly at <a href="mailto:cwa@colorado.edu">cwa@colorado.edy</a> or by phone at (303) 492-2525</p>
+          <h3>Conference on World Affairs Administration</h3>
+          <p>For questions about the conference or other events, please contact The Conference on World Affairs at <a href="mailto:cwa@colorado.edu">cwa@colorado.edy</a> or by phone at <a href="tel:303-492-2525">(303) 492-2525</a>.</p>
         </article>
         <article>
-          <h3>Website Problems/Issues</h3>
-          <p>For any issues with the Volunter Manament System Application, please contanct the VMS Team at <a href="mailto:cwajazzvms@gmail.com">cwajazzvms@gmail.com</a>.</p>
+          <h3>Website Technical Issues</h3>
+          <p>For any issues with the Volunteer Management System Application, please contact the VMS at <a href="mailto:cwajazzvms@gmail.com">cwajazzvms@gmail.com</a>.</p>
           <p>Please include a destription of your issue and any screenshots/images that display your problem.</p>
         </article>
       </section>
