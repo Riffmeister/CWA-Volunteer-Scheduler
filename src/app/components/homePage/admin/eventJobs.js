16b8a647d4627c80d3a67fd47821b6ccc8770720
dylanlcole <dylan.cole5652@gmail.com>
2017-03-20 16:13:29 -0600

    fixed admin job page to work on mobile

--- src/app/components/homePage/admin/eventJobs.js
+++ src/app/components/homePage/admin/eventJobs.js
@@ -24,7 +24,7 @@ class EventJobs extends React.Component {
     render() {
         return (
            <div className='jobs admin'>
-            <h2>Jobs</h2>
+            <h2>Jobs(Scroll to View More)</h2>
             <div className='jobs-body'>
               {this._createJobElements()}
             </div>
